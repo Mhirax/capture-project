ffd8b7251a0a3c977afd064283d1291594795fb0
Mhirax <olowookeremiracle14@gmail.com>
2026-02-25 21:36:35 +0100

    import service section

--- capture/src/pages/AboutUs.js
+++ capture/src/pages/AboutUs.js
@@ -1,6 +1,7 @@
 import React from "react";
 //page components
 import AboutSection from "../components/AboutSection";
+import ServicesSection from "../components/ServicesSection";
 
 
 const AboutUs = () => {
