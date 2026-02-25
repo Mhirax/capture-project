d7f11fa897da1ea2e0f87b2e7b3bbded6db6b5c0
Mhirax <olowookeremiracle14@gmail.com>
2026-02-25 23:22:13 +0100

    import faqsection

--- capture/src/pages/AboutUs.js
+++ capture/src/pages/AboutUs.js
@@ -2,6 +2,7 @@ import React from "react";
 //page components
 import AboutSection from "../components/AboutSection";
 import ServicesSection from "../components/ServicesSection";
+import FaqSection from "../components/FaqSection";
 
 
 const AboutUs = () => {
