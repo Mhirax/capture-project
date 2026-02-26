7fedc3c3825aa6ed414c85cdba996897e9baa63b
Mhirax <olowookeremiracle14@gmail.com>
2026-02-26 18:54:04 +0100

    add new components

--- capture/src/components/AboutSection.js
+++ capture/src/components/AboutSection.js
@@ -5,7 +5,7 @@ import styled from 'styled-components';
 
 const AboutSection = () => {
     return (
-      <div className="about">
+      <About>
         <div className="description">
           <div className="title">
             <div className="hide">
@@ -19,25 +19,27 @@ const AboutSection = () => {
             <div className="hide">
               <h2>true.</h2>
             </div>
-                </div>
-                <p>Contact us for any photography or videography ideas that you have</p>
-                <button>Contact us</button>
-            </div>
-            <div className="image">
-         <img src={home1} alt="guy with a camera" />
-            </div>
-      </div> 
+          </div>
+          <p>
+            Contact us for any photography or videography ideas that you have
+          </p>
+          <button>Contact us</button>
+        </div>
+        <div className="image">
+          <img src={home1} alt="guy with a camera" />
+        </div>
+      </About>
     );
 }
 
 // styled components
-const About = styled.div`
+const About = styled.div `
    min-height: 90vh;
    display: flex;
    align-items: center;
    justify-content: space-between;
    padding: 5rem 10rem;
    color: white;
-`
+`;
 
 export default AboutSection;
